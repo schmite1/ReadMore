37277f8ffe1b6981c1f9dc17fcabf2ec1c02f66f
schmite1 <schmite1@tcnj.edu>
2015-04-21 00:39:27 -0400

    Update modification date

--- data/whitelist.js
+++ data/whitelist.js
@@ -6,7 +6,7 @@
 // Description: A Mozilla Firefox add-on that prevents users from browsing certain sites denoted by a customizable blacklist, not by preventing site access entirely, but by redirecting to productive alternative sites for the user to explore.
 // Filename: whitelist.js
 // Description: Prototype WhiteList script. Basic functionality that randomly selects one of two URLs in an array to redirect the current webpage.
-// Last modified on: 04/14/15
+// Last modified on: 04/21/15
 
 //String of URLs separated by commas and/or spaces.
 var whiteList = "http://www.gutenberg.org/ebooks/search/?sort_order=downloads, https://en.wikipedia.org/wiki/Special:Random http://en.wikisource.org/wiki/Portal:Portals";
