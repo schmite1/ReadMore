ca9fdc4e8de3b56611292c9164c7a52d47b0037d
schmite1 <schmite1@tcnj.edu>
2015-04-21 00:38:35 -0400

    Update whiteList to demonstrate string parsing
    The WhiteList is created by parsing a single string with a regular expression, which is then passed into an array of strings. Each operation is now commented.

--- data/whitelist.js
+++ data/whitelist.js
@@ -8,7 +8,14 @@
 // Description: Prototype WhiteList script. Basic functionality that randomly selects one of two URLs in an array to redirect the current webpage.
 // Last modified on: 04/14/15
 
-var random = Math.floor(Math.random()*2)
-var whiteList = ["http://www.gutenberg.org/ebooks/search/?sort_order=downloads", "https://en.wikipedia.org/wiki/Special:Random"]
+//String of URLs separated by commas and/or spaces.
+var whiteList = "http://www.gutenberg.org/ebooks/search/?sort_order=downloads, https://en.wikipedia.org/wiki/Special:Random http://en.wikisource.org/wiki/Portal:Portals";
 
-window.location.replace(whiteList[random]);
+//Splits whiteList string on commas and spaces, then enters the separate strings into an array.
+var whiteListArray = whiteList.split(/[\s,]+/);
+
+//Generates a random number from 0 to the length of the whiteListArray -1.
+var random = Math.floor(Math.random()*(whiteListArray.length));
+
+//The current URL is replaced by a random URL from the whiteListArray.
+window.location.replace(whiteListArray[random]);
