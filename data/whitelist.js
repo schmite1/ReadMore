1cbd19a27ade0eee177b9dfca37b8e57b463ea70
schmite1 <schmite1@tcnj.edu>
2015-04-26 02:34:59 -0400

    Expound on header details
    The functionality has not been modified, but the description in the header now reflects the script in the context of the program as a whole.

--- data/whitelist.js
+++ data/whitelist.js
@@ -3,19 +3,33 @@
 // Semester: Spring 2015
 // Instructor: Dr. Pulimood
 // Project name: ReadMore
-// Description: A Mozilla Firefox add-on that prevents users from browsing certain sites denoted by a customizable blacklist, not by preventing site access entirely, but by redirecting to productive alternative sites for the user to explore.
+// Description: A Mozilla Firefox add-on that prevents users from browsing
+// 		certain sites denoted by a customizable blacklist, not by preventing
+//		site access entirely, but by redirecting to productive alternative
+//		sites for the user to explore.
 // Filename: whitelist.js
-// Description: Prototype WhiteList script. Basic functionality that randomly selects one of two URLs in an array to redirect the current webpage.
-// Last modified on: 04/21/15
+// Description: Script that creates a whitelist of sites to be redirected to
+//		when a blacklisted site (defined in blacklist.js and main.js) is
+//		navigated to. The blacklisted site is replaced with a randomly
+//		selected whitelisted websie.
+//
+//		This script functions as a proof of concept for a whitelist,
+//    parsing strings, separated by commas and/or spaces, and then
+//    populating an array with the strings, simulating user input.
+//
+// Last modified on: 04/26/15
 
 //String of URLs separated by commas and/or spaces.
-var whiteList = "http://www.gutenberg.org/ebooks/search/?sort_order=downloads, https://en.wikipedia.org/wiki/Special:Random http://en.wikisource.org/wiki/Portal:Portals";
+var whiteList = "https://en.wikipedia.org/wiki/Special:Random, "
+	+ "http://en.wikisource.org/wiki/Portal:Portals, "
+	+ "https://www.gutenberg.org/ebooks/search/?sort_order=random";
 
-//Splits whiteList string on commas and spaces, then enters the separate strings into an array.
+//Regular expression splits whiteList string on commas and spaces, then
+//		enters the separate strings into an array whiteListArray.
 var whiteListArray = whiteList.split(/[\s,]+/);
 
 //Generates a random number from 0 to the length of the whiteListArray -1.
 var random = Math.floor(Math.random()*(whiteListArray.length));
 
-//The current URL is replaced by a random URL from the whiteListArray.
+//The current browser URL is replaced by a random URL from whiteListArray.
 window.location.replace(whiteListArray[random]);
